7be6e9879ee4e06299807d3d2541d99526126d24
Elif <elffemre@gmail.com>
2022-07-04 19:47:26 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OOP_Constructor.Models;
+namespace HelloWorld
+{
+
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+          Student student = new Student();
+
+          
+           
+        }
+
+    }
+    
+}
